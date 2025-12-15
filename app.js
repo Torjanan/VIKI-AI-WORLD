42705643ccbe1cea3ef3ea64ea562372ca545710
Torjanan <tjanan223@gmail.com>
2025-12-15 15:26:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('loginBtn').onclick = () => {
+  alert('Login feature will activate in Phase 2 (Firebase)');
+};
+
+document.getElementById('signupBtn').onclick = () => {
+  alert('Signup feature will activate in Phase 2 (Firebase)');
+};
